105b86e9d40617f9a4d506ef9b985c54fb7c0477
Renan Santana Desiderio <renan_ota@hotmail.com>
2018-11-27 00:25:40 -0200

    Update Player.gd

--- Player.gd
+++ Player.gd
@@ -13,15 +13,20 @@ func _physics_process(delta):
 	# Movement
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
-		$Sprite.flip_h = false;
+		$Sprite.flip_h = false
+		$Sprite.play("run")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
-		$Sprite.flip_h = true;
+		$Sprite.flip_h = true
+		$Sprite.play("run")
 	else:
 		motion.x = 0
+		$Sprite.play("idle")
 	
 	# Jump
 	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
 		motion.y = -JUMP
+	else:
+		$Sprite.play("jump")
 	
 	motion = move_and_slide(motion, UP)
