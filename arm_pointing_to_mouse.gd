44af5dbe65a3f3151115689156c997d4b83c3d6d
Renan Santana Desiderio <renan_ota@hotmail.com>
2019-02-06 09:33:00 -0200

    Create arm_pointing_to_mouse.gd

---
+++ arm_pointing_to_mouse.gd
@@ -0,0 +1,11 @@
+var localMousePosition
+var globalMousePosition
+
+func _physics_process(delta):
+	
+	# Get local and global mouse positions
+	localMousePosition  = get_local_mouse_position()
+	globalMousePosition = get_global_mouse_position()
+	
+	# Fazer o braço apontar para o mouse
+  $Arm.rotation = localMousePosition.angle()
