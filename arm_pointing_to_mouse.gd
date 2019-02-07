22d9c6a7651d93b63a208630bdee9ed3d181991f
Renan Santana Desiderio <renan_ota@hotmail.com>
2019-02-07 00:35:04 -0200

    Update arm_pointing_to_mouse.gd

--- arm_pointing_to_mouse.gd
+++ arm_pointing_to_mouse.gd
@@ -1,14 +1,18 @@
-var localMousePosition
-var globalMousePosition
-
 func _physics_process(delta):
 	
-	# Get local and global mouse positions
-	localMousePosition  = get_local_mouse_position()
-	globalMousePosition = get_global_mouse_position()
-	
 	# Fazer o braço apontar para o mouse
-  	$Arm.rotation = localMousePosition.angle()
-
-	# Correção quando o mouse está pra tras do braço
+	$Arm.rotation = get_local_mouse_position().angle()
 	
+	# Checar se o player esta olhando pra esquerda ou direita
+	if position.x < get_global_mouse_position().x:
+		lookRight = 1
+	else:
+		lookRight = 0
+		
+	# Flip nos sprites quando o personagem vira
+	if lookRight:
+		$Sprite.set_scale(Vector2(1, 1))
+		$Arm.set_scale(Vector2(1.5, 0.5))
+	else:
+		$Sprite.set_scale(Vector2(-1, 1))
+		$Arm.set_scale(Vector2(1.5, -0.5))
